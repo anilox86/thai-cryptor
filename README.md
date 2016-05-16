b47b69cbf09209ed54f5493f1b8e0460b159bb4a
Panupon Chackramanon <pncksk@gmail.com>
2016-05-17 00:06:34 +0700

    New feature: encrypt (thai to english), improve performance (?)

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # Thai Cryptor
-Thai Cryptor - English to Thai character replacer based on keyboard layout written in Python 3
+Thai Cryptor - English-Thai character replacer based on keyboard layout written in Python 3
 
 ### Features
-- replace thai character from english strings based on keyboard layout
+- decrypt: replace thai character from english strings
+- encrypt: replace english character from thai strings
 
 ### Requirements
 - Python3
 
 ### Usage
 - add text you want to decrypt to in.txt
-- run this script
+- run this script (depend on your system, see below)
 - check your output in out.txt
 
-**Linux**
-- `./thai-cryptor.py`
-
 **Windows**
-- right-click on start.ps1 and click 'Run with PowerShell' if python.exe is not at C:\\Python34 you need to edit to correct path yourself
+- decrypt: right-click on decrypt.ps1 and click 'Run with PowerShell'
+- encrypt: right-click on encrypt.ps1 and click 'Run with PowerShell'
+- if python.exe is not in C:\\Python34 you need to edit to correct path yourself
+
+**Linux**
+- `./thai-cryptor.py <command> [<file>] ...`
+- 2 commands: `decrypt` or `encrypt`
+- you can also specify file(s) yourself like `./thai-cryptor.py decrypt mytext1.txt`
 
 ### Todo
-- replace from Thai to English
 - test on Windows
--- thai-cryptor.py
+++ thai-cryptor.py
@@ -1,61 +1,117 @@
 #!/usr/bin/python3
 
-def replace_all(text, dic):
+import sys
+
+def decrypt_eng(text, dic):
     for i, j in dic.items():
         text = text.replace(i, j)
     return text
 
+def encrypt_eng(text, dic):
+    for i, j in dic.items():
+        text = text.replace(j, i)
+    return text
+
+def print_usage():
+    print("Usage: thai-cryptor <command> [<file1>] [<file2>] ...\n\
+\n\
+command:\n\
+    decrypt     replace english to thai\n\
+    encrypt     replace thai to english\n\
+\n\
+Examples:\n\
+    ./thai-cryptor decrypt\n\
+    ./thai-cryptor encrypt sometextfile.txt\n\
+    ./thai-cryptor decrypt file1.txt file2.txt file3.txt\n\
+\n\
+Then check your output in out.txt")
+    return
+
+def dec(list_in):
+    write_file = open("out.txt", "w")
+    try:
+        for i in list_in:
+            with open(i, "r") as fan:
+                lines = fan.read().splitlines()
+
+            write_file.write("file: " + i + "\n")
+            for line in lines:
+                txt = decrypt_eng(line, reps)
+                write_file.write(txt + "\n")
+            write_file.write("\n")
+    except:
+        print("Error: file not found(?)")
+    finally:
+        write_file.close()
+
+def enc(list_in):
+    write_file = open("out.txt", "w")
+    try:
+        for i in list_in:
+            with open(i, "r") as fan:
+                lines = fan.read().splitlines()
+
+            write_file.write("file: " + i + "\n")
+            for line in lines:
+                txt = encrypt_eng(line, reps)
+                write_file.write(txt + "\n")
+            write_file.write("\n")
+    except:
+        print("Error: file not found(?)")
+    finally:
+        write_file.close()
+
 reps = {'A':'ฤ',
-        'B':'\ฺ',
+        'B':'ฺ',
         'C':'ฉ',
         'D':'ฏ',
         'E':'ฎ',
         'F':'โ',
         'G':'ฌ',
-        'H':'\็',
+        'H':'็',
         'I':'ณ',
-        'J':'\๋',
+        'J':'๋',
         'K':'ษ',
         'L':'ศ',
         'M':'?',
-        'N':'\์',
+        'N':'์',
         'O':'ฯ',
         'P':'ญ',
         'Q':'๐',
         'R':'ฑ',
         'S':'ฆ',
         'T':'ธ',
-        'U':'\๊',
+        'U':'๊',
         'V':'ฮ',
         'W':'"',
         'X':')',
-        'Y':'\ํ',
+        'Y':'ํ',
         'Z':'(',
         'a':'ฟ',
-        'b':'\ิ',
+        'b':'ิ',
         'c':'แ',
         'd':'ก',
         'e':'ำ',
         'f':'ด',
         'g':'เ',
-        'h':'\้',
+        'h':'้',
         'i':'ร',
-        'j':'\่',
+        'j':'่',
         'k':'า',
         'l':'ส',
         'm':'ท',
-        'n':'\ื',
+        'n':'ื',
         'o':'น',
         'p':'ย',
         'q':'ๆ',
         'r':'พ',
         's':'ห',
         't':'ะ',
-        'u':'\ี',
+        'u':'ี',
         'v':'อ',
         'w':'ไ',
         'x':'ป',
-        'y':'\ั',
+        'y':'ั',
         'z':'ผ',
         '0':'จ',
         '1':'ๅ',
@@ -63,11 +119,11 @@ reps = {'A':'ฤ',
 #        '3':'-',
         '4':'ภ',
         '5':'ถ',
-        '6':'\ุ',
-        '7':'\ึ',
+        '6':'ุ',
+        '7':'ึ',
         '8':'ค',
         '9':'ต',
-        '^':'\ู',
+        '^':'ู',
         '-':'ข',
         '=':'ช',
         ',':'ม',
@@ -87,23 +143,35 @@ reps = {'A':'ฤ',
 #        '\\':'ฃ',
         '{':'ฐ'
         }
-sp = { '\\':'' }
-
 
 def main():
-    write_file = open("out.txt", "w")
-    try:
-        with open("in.txt", "r") as fan:
-            lines = fan.read().splitlines()
+    if(len(sys.argv) < 2):
+        print_usage()
+        return
 
-        for line in lines:
-            txt = replace_all(line, reps)
-            txt2 = replace_all(txt, sp)
-            write_file.write(txt2 + "\n")
-    except:
-        print("please create in.txt and put text you want to decrypt in it")
-    finally:
-        write_file.close()
+    elif(sys.argv[1] == 'decrypt'):
+        if(len(sys.argv) > 2):
+            ins = sys.argv[2:]
+            for i in ins:
+                print(i)
+        else: # len == 2
+            ins = ["in.txt"]
+        dec(ins)
+        return
+
+    elif(sys.argv[1] == 'encrypt'):
+        if(len(sys.argv) > 2):
+            ins = sys.argv[2:]
+            for i in ins:
+                print(i)
+        else: # len == 2
+            ins = ["in.txt"]
+        enc(ins)
+        return
+
+    else:
+        print_usage()
+        return
 
 
 if(__name__ == "__main__"):
